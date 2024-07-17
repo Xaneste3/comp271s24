2767d3a1854552c9fad2b2d2a973b6e886f2f077
Xander Estevez <167257083+Xaneste3@users.noreply.github.com>
2024-07-17 21:04:18 +0000

    Final Homework #7

--- Assignments/Assignment_07/SimpleLinkedList.java
+++ Assignments/Assignment_07/SimpleLinkedList.java
@@ -4,6 +4,81 @@
  */
 public class SimpleLinkedList implements Stack271<String>, Queue271<String> {
 
+    /** The first node of the linked list */
+    private Node head;
+
+    /** The last node of the linked list */
+    private Node tail;
+
+    /**Initializes the SimpleLinkedList */
+    public SimpleLinkedList() {
+    this.head = null;
+    this.tail = null;
+    }
+
+    /** 
+     * 
+     * Adds string to the end of the SimpleLinkedList
+     * 
+     * @param e is the String I use
+     * @return will be true if String is added to the end correctly
+      */
+    public boolean add(String e) {
+        Node testNode = new Node(e);
+
+        if(head == null) {
+            head = testNode;
+            tail = testNode;          
+        }
+        else {
+            tail.setNext(testNode);
+            tail = testNode;
+        }
+        return true;
+    }
+
+    /**
+     * This method will remove the beginning element in SimpleLinkedList
+     * 
+     * @return the string that got removed at the beginning or returns null if there is nothing
+     */
+    public String remove() {
+        String deadData = null;
+        if (head != null) {
+            deadData = head.toString();
+            head = head.getNext();
+            
+            if(head == null) {
+                tail = null;
+            }
+        }
+        return deadData;  
+    }
+    
+    public String pull() {
+        return remove();
+    } 
+
+    /**
+     * This method will push(add) a string to the beginning of the SimpleLinkedList
+     * 
+     * @param e will be the String that gets added
+     * @return will be true if the String gets pushed correctly
+     */
+    public boolean push(String e) {
+        Node testNode = new Node(e);
+        if(head == null) {
+            head = testNode;
+            tail = testNode;
+        }
+        else {
+            testNode.setNext(head);
+            head = testNode;
+
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         SimpleLinkedList demoQueue = new SimpleLinkedList();
         SimpleLinkedList demoStack = new SimpleLinkedList();
@@ -29,49 +104,5 @@ public class SimpleLinkedList implements Stack271<String>, Queue271<String> {
         System.out.println(queueWorks);
         System.out.println(stackWorks);
     }
-    
-    /** The first node of the linked list */
-    private Node head;
-
-    /** The last node of the linked list */
-    private Node tail;
-
-    private int usage;
-
-    public SimpleLinkedList() {
-    this.head = null;
-    this.tail = null;
-    this.usage = 0;
-    }
-
-    public boolean add(String e) {
-        Node testNode = new Node(e);
-
-        if(head.equals(null)) {
-            head = testNode;
-            tail = testNode;          
-        }
-        else {
-            tail.setNext(testNode);
-            tail = testNode;
-        }
-        usage++;
-        return true;
-    }
-
-    public String remove() {
-        String deadData = null;
-        if (head != null) {
-            deadData = head.toString();
-            head = head.getNext();
-            
-            if(head.equals(null)) {
-                tail = null;
-            }
-            usage--;
-        }
-        return deadData;  
-    }
-    
 
 }// class SimpleLinkedList
